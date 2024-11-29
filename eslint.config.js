e2b35bc164a90a95883768384e3d11a1b1f900df
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-11-29 10:30:55 -0300

    configurando o eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+const react = require('eslint-plugin-react');
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    extends: ['prettier'],
+    rules: {
+      // ... any rules you want
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+     },
+    // ... others are omitted for brevity
+  },
+];
