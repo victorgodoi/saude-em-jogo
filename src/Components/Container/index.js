75bb7042c2834adec1a2801e2bbe11e4c037c48c
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-11-30 11:28:57 -0300

    Atualizando o title das páginas

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -1,6 +1,27 @@
+import { useEffect } from 'react';
 import './Container.scss';
 
 function Container({ children }) {
+  useEffect(() => {
+    document.title = "Saúde em Jogo";
+  }, []);
+
+  //useEffect(() => {
+  // const changeFavicon = (url) => {
+  //  const link = document.querySelector("link[rel~='icon']");
+  //  if (link) {
+  //    link.setAttribute("href", url);
+  //  } else {
+  //    const newLink = document.createElement("link");
+  //    newLink.rel = "icon";
+  //    newLink.href = url;
+  //    document.head.appendChild(newLink);
+  //  }
+  //};
+
+  // changeFavicon("../../Images/FavIconSEJ.png");
+  // }, []);
+
   return <div className='conteudo'>{children}</div>
 };
 
