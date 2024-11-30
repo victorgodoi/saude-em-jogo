e23d2496273956128701ea61885c0f5b569110cd
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-11-30 12:36:18 -0300

    Atualizando favicon e adicionando componentes na tela de login

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -1,28 +1,6 @@
-import { useEffect } from 'react';
 import './Container.scss';
-import FavIconSEJ from '../../Images/FavIconSEJ.png';
 
 function Container({ children }) {
-  useEffect(() => {
-    document.title = "Saúde em Jogo";
-  }, []);
-
-  useEffect(() => {
-    const changeFavicon = (url) => {
-      const link = document.querySelector("link[rel~='icon']");
-      if (link) {
-        link.setAttribute("href", url);
-      } else {
-        const newLink = document.createElement("link");
-        newLink.rel = "icon";
-        newLink.href = url;
-        document.head.appendChild(newLink);
-      }
-    };
-
-    changeFavicon(FavIconSEJ);
-  }, []);
-
   return <div className='conteudo'>{children}</div>
 };
 
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,16 @@
 import Header from '../../Components/Header';
 import './Home.scss';
+import Logo from '../../Images/Logo.svg';
 
 function Home() {
   return (
     <div className='home'>
       <Header text={''} />
+      <div id='login'>
+        <p>SAÚDE EM JOGO</p>
+        <img src={Logo} alt='Cabeça com quebra-cabeça' title='Cabeça com quebra-cabeça' />
+        <input placeHolder='Digite seu nome' />
+      </div>
     </div>
   );
 }
