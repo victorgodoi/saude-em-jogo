86d2c6c22997123d3ea07905953cbe72be988678
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-12-02 14:41:01 -0300

    Ajuste no header e alteração na img do ícone da festa

--- src/Components/BoxDiagnosis/index.js
+++ src/Components/BoxDiagnosis/index.js
@@ -1,5 +1,5 @@
 import './BoxDiagnosis.scss';
-import IconParty from '../../Images/iconeFesta.png';
+import IconParty from '../../Images/IconParty.PNG';
 import IconMedalha from '../../Images/IconMedalha.svg';
 
 function BoxDiagnosis({ title, text }) {
@@ -11,7 +11,7 @@ function BoxDiagnosis({ title, text }) {
       <div className='card'>
         <div>
           <p>Parabéns!</p>
-          <img src={IconParty} alt='ícone de festa' title='ícone de festa' />
+          <img id='party' src={IconParty} alt='ícone de festa' title='ícone de festa' />
         </div>
         <p>{text}</p>
         <img src={IconMedalha} alt='ícone de medalha' title='ícone de medalha' />
