135d77a0500b3e795e7715ea246b4aa372fcd485
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-12-03 20:27:37 -0300

    Atualizando função da tela de login e alguns ajustes de css

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,8 +1,14 @@
 import './Button.scss';
 import { Link } from "react-router-dom";
 
-function Button({ text, color, url }) {
-  return <Link to={url}><button style={{ backgroundColor: color }} >{text}</button></Link >
-};
+function Button({ text, color, url, onClick }) {
+  return (
+    <Link to={url}>
+      <button onClick={onClick} style={{ backgroundColor: color }}>
+        {text}
+      </button>
+    </Link>
+  );
+}
 
 export default Button;
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -2,8 +2,31 @@ import './Login.scss';
 import Logo from '../../Images/Logo.svg';
 import ImgHeader from '../../Images/ImgHeader.png';
 import Button from '../../Components/Button';
+import { useEffect, useState } from 'react';
 
 function Login() {
+  const [valueInput, setValueInput] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleInputChange = (event) => {
+    setValueInput(event.target.value);
+    setError(false);
+  };
+
+  useEffect(() => {
+    localStorage.removeItem("name");
+  }, []);
+
+  const handleButtonClick = () => {
+    if (!valueInput.trim()) {
+      setError(true);
+      return;
+    }
+
+    localStorage.setItem("name", valueInput);
+    window.location.href = '/home';
+  };
+
   return (
     <div className='pageLogin'>
       <div className='contentLogin'>
@@ -15,8 +38,16 @@ function Login() {
             <p>SAÚDE EM JOGO</p>
             <img src={Logo} alt='Cabeça com quebra-cabeça' title='Cabeça com quebra-cabeça' />
             <div>
-              <input placeHolder='Digite seu nome' />
-              <Button url='/home' color='#FFDE00' text='Entrar' />
+              <input
+                placeholder='Digite seu nome'
+                value={valueInput}
+                onChange={handleInputChange}
+                style={{
+                  border: error ? '2px solid red' : '1px solid #ccc',
+                }}
+              />
+              {error && <p id="mensageError">Preencha o nome antes de entrar</p>}
+              <Button onClick={handleButtonClick} color='#FFDE00' text='Entrar' />
             </div>
           </div>
         </div>
