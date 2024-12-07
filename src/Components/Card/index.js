b40c2ccb4b202eea8cb82e2cab9e2cd0823099ad
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-12-07 14:03:56 -0300

    Ajustando estilo dos cards

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,4 +1,6 @@
 import './Card.scss';
+import Logo from '../../Images/Logo.svg';
+
 
 export function Card({ flipped = false, back, handleClick, id }) {
   const cardContentClassNames = ['card__content'];
@@ -15,7 +17,7 @@ export function Card({ flipped = false, back, handleClick, id }) {
   return (
     <div className="card" onClick={() => handleClickFn(id)}>
       <div className={cardContentClassNames.join(' ')}>
-        <div className="card__face card__face--front">?</div>
+        <div className="card__face card__face--front"><img src={Logo} alt='Cabeça com quebra-cabeça' title='Cabeça com quebra-cabeça' /></div>
         <div className="card__face card__face--back">{back}</div>
       </div>
     </div>
