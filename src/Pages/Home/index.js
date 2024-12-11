5febd4d6106dfc27bd1d1bda9d9270ba6610647b
Victor de Godoi Coelho <120597356+victorgodoi@users.noreply.github.com>
2024-12-11 18:57:49 -0300

    alterar texto do home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -14,7 +14,7 @@ function Home() {
           <div className='divHome'>
             <p className='titleHome'>Estimule sua memória, divirta-se e cuide da sua mente!</p>
             <p className='welcome'>Seja Bem - Vindo {nameUser}!</p>
-            <p className='textHome'>Desenvolvemos uma plataforma interativa especialmente para idosos, com o objetivo de estimular a memória e promover a saúde mental de forma lúdica e envolvente. Inspirada no universo dos Simpsons, nosso jogo da memória temático convida você a combinar imagens dos personagens dessa série nostálgica e familiar, criando momentos de diversão e conexão.</p>
+            <p className='textHome'>Desenvolvemos uma plataforma interativa especialmente para idosos, com o objetivo de estimular a memória e promover a saúde mental de forma lúdica e envolvente. Nosso jogo da memória temático convida você a combinar imagens de emojis relacionados à saúde, criando momentos de diversão e conexão.</p>
             <p className='subTitleHome' >Por Que Jogar?</p>
             <ul>
               <li><p>Estimulação Cognitiva:</p>Ajuda a manter a memória ativa e a agilidade mental.</li>
